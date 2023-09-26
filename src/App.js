7a2f2f902e0078fd9c1f55cd64fa3c9f010d23b6
Robert Jay Castillo Quimno <rjcquimno@addunet.ph>
2023-09-26 19:37:31 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import TextInput from './components/TextInput';
+import CheckBox from './components/CheckBox';
+import Header from './components/Header';
+import Category from './components/Category';
+import Items from './components/Items';
+import { useState } from 'react';
+
+const sportingGoods = [
+  {id: 1, name: 'Football',price: 49.99, stock: 0},
+  {id: 2, name: 'Baseball',price: 9.99, stock: 1},
+  {id: 3, name: 'Basketball',price: 29.99, stock: 2}
+]
 
 function App() {
+  const [txtValue, setTxtValue] = useState('')
+  const [form, setForm] = useState({
+    nameItem: '',
+    priceItem: 0,
+    stockItem: 0,
+  })
+  const [showOnlyStock,setShowOnlyStock] = useState(false)
+  const [data, setData] = useState(sportingGoods)
+
+const handleClick = () =>{
+  alert('Clicked')
+}
+
+const handleForm = (e) => setForm({...form, [e.target.id]: e.target.value})
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(form)
+
+  setData([...data, {id: data.length + 1, name: form.nameItem, price: form.priceItem, stock: form.stockItem}])
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form style={{display:'flex', flexDirection: 'column', gap: 10, marginBottom: 20}} onSubmit={handleSubmit}> 
+      <div>
+        <label htmlFor='nameItem'>Name:</label>
+        <input  id='nameItem' type ='text' value={form.nameItem} onChange={handleForm}/>
+        </div>
+      <div>
+        <label htmlFor='priceItem'>Price:</label>
+        <input  id='priceItem' type ='number' value={form.priceItem} onChange={handleForm}/>
+        </div>
+      <div>
+        <label htmlFor='stockItem'>Stock:</label>
+        <input  id='stockItem' type ='number' value={form.stockItem} onChange={handleForm}/>
+      </div>
+
+        <button type='submit'>Submit</button>
+
+      </form>
+
+        <TextInput value={txtValue} onChange={(e) => setTxtValue(e.target.value)}/>
+        <CheckBox checked={showOnlyStock} onChange={(e) => setShowOnlyStock(e.target.checked)}/>
+        <button onClick={handleClick}>Submit</button>
+
+        <table>
+          <tbody>
+          <Header/>
+          <Category/>
+          <Items items = {data} includePrice query={txtValue} showOnlyStock={showOnlyStock}/>
+          <tr>
+            <td colSpan="2" style ={{textAlign: 'right' ,fontWeight:'bold'}}>Total</td>
+            <td style ={{paddingLeft: '50px' ,fontWeight:'bold'}}>{data.reduce((total, sportingGood) => {return total + parseInt(sportingGood.stock)}, 0)}</td>
+          </tr>
+
+          </tbody>
+
+        </table>
     </div>
   );
 }
---
+++ src/components/Category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <tr>
+      <td style={{fontWeight:'bold'}}>Sporting Goods</td>
+    </tr>
+  )
+}
+
+export default Category
